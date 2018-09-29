1fdbd32e96ade6b5568c10013e6a0394382967f4
Ryan Gill <rdgill95@gmail.com>
2018-09-29 14:44:53 +0000

    automatically backed up by learn

--- hello.rb
+++ hello.rb
@@ -1,5 +1,5 @@
 puts "Hello, World";
-puts "Hey there.";
-puts "My name is Ryan";
-puts "I am from Washington DC";
-puts "I'm so excited to be here!"
+puts "Hey there."; #Greeting
+puts "My name is Ryan";#name
+puts "I am from Washington DC"; #Where I am from
+puts "I'm so excited to be here!" #Message
