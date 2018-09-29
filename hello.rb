f98b9ad0a224e45bc95da8c7d1ef9a1d0b66e3a7
Ryan Gill <rdgill95@gmail.com>
2018-09-29 15:19:41 +0000

    Done.

---
+++ hello.rb
@@ -0,0 +1,4 @@
+puts "Hey there."; #Greeting
+puts "My name is Ryan";#name
+puts "I am from Washington DC"; #Where I am from
+puts "I'm so excited to be here!" #Message
---
+++ variables.rb
@@ -0,0 +1,7 @@
+name = "Ryan";
+
+number = 23 /6;
+
+puts name + " Is number " + number.to_s
+
+puts sentence 
